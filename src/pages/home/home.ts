62f3678e7ca96894b3bec4ac77b45e17626fa34b
Rafael Díaz <palancana.bcn@gmail.com>
2018-04-30 22:14:20 +0200

    Added searchbar

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
+import { empty } from 'rxjs/Observer';
+import * as $ from 'jquery';
+import { asPureExpressionData } from '@angular/core/src/view';
 declare var require: any;
 var wdk = require("wikidata-sdk");
 var request = require('request');
+var inputSearchValue = 'empty';
 
 @Component({
   selector: 'page-home',
@@ -15,37 +19,71 @@ export class HomePage {
   constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
   }
   
-  onDoAction () {
-    let alert = this.alertCtrl.create({
+  onDoActionButton () {
+    var inputValue = "hello"
+    //inputValue = (<HTMLInputElement>document.getElementById('inputSearchBox')).value;
+    console.log(inputValue);
+    /*let alert = this.alertCtrl.create({
       title: 'Mensaje de alerta!',
-      subTitle: 'ALERTA ALERTA ALERTA',
+      subTitle: inputValue,
       buttons: ['OK']
     });
     alert.present();
+    */
 
   }
 
-  onDoQuery () {
+ 
+
+  onSearchInput (ev:any) {
+    
+    // set val to the value of the searchbar
+    inputSearchValue = ev.target.value;
+    console.log(inputSearchValue);
+
+  }
+
+  changeColor(){
+    $('#myButton').text('white');
+  }
+
+  onSearchWikidataButton ()   {
     //Get entities from a string (If the values aren't available in the 
     //desired language, it will fallback to the English value if available)
-    const url = wdk.searchEntities({
-      search: 'Steve Jobs',
+var url="hello";
+     url = wdk.searchEntities({
+      search: inputSearchValue,
       language: 'ca',
       limit: '1',
       format: 'json'
     })
-    console.log(url);
 
-    const jsondata = request({
-      url: url,
-      json: true
-    }, function (error, response, body) {
-      if (!error && response.statusCode === 200) {
-        body;
-      }
-    });
+ /*   console.log(url);
+    var aux;
+    var getJSON = function(url, callback) {
+      debugger;
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', url, true);
+      xhr.responseType = 'json';
+      xhr.onload = function() {
+        var status = xhr.status;
+        if (status === 200) {
+          debugger;
 
-    console.log(jsondata);
+          callback(null, xhr.response);
+        } else {
+          debugger;
+          callback(status, xhr.response);
+        }
+      };
+      xhr.send();
+  };
+  console.log(getJSON);
+  $(#miuiusis).val(aux.Vale)
+aux.Value.nombre
   }
 
+  */
+
+}
 }
